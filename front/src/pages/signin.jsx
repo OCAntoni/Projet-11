853b6baaec2da29c782875f076722d4be0e6a8c7
Antoni Castagne <anto.casatgne@hotmail.fr>
2024-01-18 16:29:16 +0100

    suite intégration page connexion partie front

--- front/src/pages/signin.jsx
+++ front/src/pages/signin.jsx
@@ -1,10 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Nav from "../components/nav/nav"
+import Footer from "../components/footer/footer"
 import "../styles/main.scss"
 
 function Signin() {
     return (
-        <div>
+        <div className="body">
             <Nav />
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <FontAwesomeIcon icon="fa-solid fa-circle-user" />
+                    <h1>Sign In</h1>
+                    <form>
+                        <div className="input-wrapper">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username" />
+                        </div>
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" />
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div>
+                        <button className="sign-in-button">Sign In</button>
+                    </form>
+                </section>
+            </main>
+            <Footer />
         </div>
     )
 }
